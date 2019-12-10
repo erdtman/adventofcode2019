a44cbd3c7ff27b04a9bc252d4fd088cf349ed2ab
Samuel Erdtman <erdtman@spotify.com>
2019-12-10 18:21:34 +0100

    removed comments

--- day6/js/day6a.js
+++ day6/js/day6a.js
@@ -5,14 +5,10 @@ const orbits = fs.readFileSync('day6/js/input', 'utf-8')
     .filter(value => value !== '')
     .map(value => value.split(')'));
 
-// find root
-
 function countOrbits(currentNode, valueOfOrbit) {
-    //console.log(`at ${currentNode} with value ${valueOfOrbit}`);
     
     const externalOrbitsCount = orbits
         .filter(value => currentNode === value[0])
-        //.map(value => {console.log(value); return value})
         .map(nextLevel => countOrbits(nextLevel[1], valueOfOrbit + 1))
         .reduce((a, b) => a + b, 0);
     return externalOrbitsCount + valueOfOrbit;
