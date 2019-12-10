618bb925c79c08cf030ea19bdf04e1e18b30b06c
Samuel Erdtman <erdtman@spotify.com>
2019-12-10 16:55:30 +0100

    solved day 4 part 1 and 2

---
+++ day4/js/day4b.js
@@ -0,0 +1,33 @@
+
+const lowerBound = 246540;
+const upperBound = 787419;
+
+
+const validPasswords = []
+for (let index = lowerBound-1; index <= upperBound; index++) {
+    const numbers = Array.from(String(index), Number);
+
+    // has adjcent numbers
+    if (!(                           numbers[0] === numbers[1] && numbers[1] !== numbers[2] ||
+        numbers[0] !== numbers[1] && numbers[1] === numbers[2] && numbers[2] !== numbers[3] ||
+        numbers[1] !== numbers[2] && numbers[2] === numbers[3] && numbers[3] !== numbers[4] ||
+        numbers[2] !== numbers[3] && numbers[3] === numbers[4] && numbers[4] !== numbers[5] ||
+        numbers[3] !== numbers[4] && numbers[4] === numbers[5])) {
+            continue;
+    }
+
+    // always increasing 
+    if (!(numbers[0] <= numbers[1] &&
+        numbers[1] <= numbers[2] &&
+        numbers[2] <= numbers[3] &&
+        numbers[3] <= numbers[4] &&
+        numbers[4] <= numbers[5])) {
+            continue;
+    }
+    
+    validPasswords.push(index);
+}
+
+console.log(validPasswords);
+console.log(validPasswords.length);
+
